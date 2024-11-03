b86ca1cc548e5b9876ca6369dae7f8aaddfa7aaa
Secri <44509048+Secri@users.noreply.github.com>
2024-11-03 21:12:04 +0100

    Update FormValidation.js

--- FormValidation.js
+++ FormValidation.js
@@ -1,8 +1,8 @@
 class FormValidation {
+			
+				static eltHandled = ['input', 'textarea', 'fieldset'];
 				
-				static eltHandled    = ['input', 'textarea', 'fieldset'];
-				
-				static usableRegex   = { //Expression régulières à utiliser pour le match
+				static usableRegex   = { //Expression régulières à utiliser pour le match - Les propriétés servent aussi à check les types d'input gérés par le script
 											    text: /^[ -~¡-ÿ]+$/, //Caractères unicodes acceptés pour les langues latines
 											   email: /^([A-Z0-9_+-]+\.?)*[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i,
 											password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])\S{8,}$/,
@@ -11,7 +11,7 @@ class FormValidation {
 											     url: /^(http(s?):\/\/)?(www\.)?[a-zA-Z0-9\.\-\_]+(\.[a-zA-Z]{2,3})+(\/[a-zA-Z0-9\_\-\s\.\/\?\%\#\&\=]*)?$/,
 											     tel: /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/
 									   };
-									   
+				
 				static errorMessages = { //Messages d'erreur personnalisés
 											    text: 'L\'un des caractères saisis est invalide',
 											   email: 'L\'email saisi est invalide',
@@ -26,7 +26,6 @@ class FormValidation {
 									   
 				elementCollection = [];
 				validFields       = [];
-				cantBeBlank       = [];
 				invalidFields     = [];
 				
 				constructor( form ) {
@@ -66,23 +65,20 @@ class FormValidation {
 						/* For verification purpose */
 						console.log(this.elementCollection);
 						console.log(this.validFields);
-						console.log(this.cantBeBlank);
 						console.log(this.invalidFields);
 						
 						this.clearAll(); //On clear les statuts invalides et les messages d'erreur
 						
-						if (this.cantBeBlank.length > 0 || this.invalidFields.length > 0) {
+						if (this.invalidFields.length > 0) {
 							event.preventDefault(); //On bloque la soumission du formulaire
 							console.log('submission prevented'); //For verification purpose
-							this.displayErrors(this.cantBeBlank);
 							this.displayErrors(this.invalidFields);
 						}
 						
 						this.validFields = [];
-						this.cantBeBlank = [];
 						this.invalidFields = [];
 					});
-					
+				
 				}
 				
 				collectFormElt() {
@@ -94,13 +90,13 @@ class FormValidation {
 							}
 						}
 					}
-					console.log(this.elementCollection);
+					console.log(this.elementCollection); //For verification purpose
 				}
 				
 				blankCheck(element) {
 					element.value = element.value.trim();
 					if (element.value.length === 0 && element.getAttribute('aria-required') === 'true') {
-						this.cantBeBlank.push(element);
+						this.invalidFields.push([element, 'empty']);
 						return true;
 					}
 					return false;
@@ -140,52 +136,47 @@ class FormValidation {
 							i++;
 							let errorMsg = document.createElement('span');//Création de l'élément qui va contenir le msg
 							errorMsg.classList.add('errorMsg');
-							if ( Array.isArray( field ) ) { //Si le champ est un tableau alors l'erreur vient d'un match regex
-								errorMsg.setAttribute('id', 'invalid_' + field.nodeName + i); //Attribution de l'id
-								if (field[1] === 'regex') {
+							errorMsg.setAttribute('id', 'invalid_' + field.nodeName + i); //Attribution de l'id
+							switch (field[1]) {
+								case 'regex':
 									errorMsg.textContent = FormValidation.errorMessages[ field[0].type ];//On va chercher le message d'erreur dans la propriété statique errorMessages et on l'injecte
-								} else if (field[1] === 'check') {
+									break;
+								case 'empty':
+									errorMsg.textContent = FormValidation.errorMessages['empty'];
+									break;
+								case 'check':
 									errorMsg.textContent = FormValidation.errorMessages['check'];
-								}
-								field[0].insertAdjacentElement('afterend', errorMsg); //On ajoute le message juste après l'élément invalide
-								field[0].setAttribute('aria-invalid', 'true'); //On ajoute l'attribut aria-invalid
-								field[0].setAttribute('aria-errormessage', 'invalid_' + field.nodeName + i); //On ajoute l'attribut aria-error message qui se réfère à l'id de l'élément invalide
-								this.correctionListener(field[0]);
-								
-							} else { // Sinon il s'agit d'un champ required vide
-								errorMsg.setAttribute('id', 'blank_' +  field.nodeName + i);
-								errorMsg.textContent = FormValidation.errorMessages['empty'];
-								field.insertAdjacentElement('afterend', errorMsg);
-								field.setAttribute('aria-invalid', 'true');
-								field.setAttribute('aria-errormessage', 'blank_' +  field.nodeName + i);
-								this.correctionListener(field);
+									break;
 							}
+							field[0].insertAdjacentElement('afterend', errorMsg); //On ajoute le message juste après l'élément invalide
+							field[0].setAttribute('aria-invalid', 'true'); //On ajoute l'attribut aria-invalid
+							field[0].setAttribute('aria-errormessage', 'invalid_' + field.nodeName + i); //On ajoute l'attribut aria-error message qui se réfère à l'id de l'élément invalide
+							this.correctionListener(field[0]);
 						}
 					}
 					return;
 				}
 				
 				correctionListener(element) {
-					if (element.tagName != 'FIELDSET') {
+					if (element.tagName === 'FIELDSET') {
+						element.addEventListener('change', (event) => {
+							this.removeError(element);
+						});
+					} else {
 						let currentContent = element.value.trim();
 						element.addEventListener('input', (event) => {
 							if (element.value.trim().length === 0 || element.value.trim() != currentContent ) {
-								element.removeAttribute('aria-invalid');
-								element.removeAttribute('aria-errormessage');
-								if (element.nextElementSibling && element.nextElementSibling.classList.contains('errorMsg')) {
-									element.nextElementSibling.remove();
-								}
+								this.removeError(element);
 							}
 						});
 					}
-					if (element.tagName === 'FIELDSET') {
-						element.addEventListener('change', (event) => {
-							element.removeAttribute('aria-invalid');
-							element.removeAttribute('aria-errormessage');
-							if (element.nextElementSibling && element.nextElementSibling.classList.contains('errorMsg')) {
-								element.nextElementSibling.remove();
-							}
-						});
+				}
+				
+				removeError(element) {
+					element.removeAttribute('aria-invalid');
+					element.removeAttribute('aria-errormessage');
+					if (element.nextElementSibling && element.nextElementSibling.classList.contains('errorMsg')) {
+						element.nextElementSibling.remove();
 					}
 				}
 				
@@ -199,9 +190,9 @@ class FormValidation {
 						msg.remove();
 					}
 				}
-				
-			}			
 			
+			}
+						
 			/*****************************************************************/
 			var contactForms  = document.querySelectorAll('form'); //Collection de tous les formulaires de la page
 			var formInstances = {}; //Collection des futures instances FormValidation
